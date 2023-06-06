ccd8f1fc87e399aa5ecf009b2b133d5c7cbd95bc
Aziz Limonu <azizlimonu@gmail.com>
2023-06-06 16:26:36 +0800

    add submit review feature

--- components/productPage/review/AddReview.js
+++ components/productPage/review/AddReview.js
@@ -4,6 +4,15 @@ import React, { useState } from 'react';
 import { Rating } from "@mui/material";
 import Select from './Select';
 import Images from './Images';
+import { useDispatch } from 'react-redux';
+import { uploadImages } from '../../../libs/upload';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { useEffect } from 'react';
+import DialogModal from '../../dialogmodal';
+import DotLoaders from '../../loaders/DotLoaders';
+import dataURItoBlob from '../../../utils/dataURItoBlob';
+import { hideDialog, showDialog } from "../../../store/dialogSlice";
 
 const AddReview = ({ product, setReviews }) => {
   const [loading, setLoading] = useState(false);
@@ -16,15 +25,102 @@ const AddReview = ({ product, setReviews }) => {
 
   let fits = ["Small", "True to size", "Large"];
 
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(hideDialog());
+  }, []);
+
   let uploaded_images = [];
   const handleSubmit = async () => {
-    console.log("Submitted")
-  }
+    try {
+      setLoading(true);
+      let messages = [];
+      if (!size) {
+        messages.push({
+          msg: "Please select a size !",
+          type: "error",
+        });
+        return;
+      }
+      if (!style) {
+        messages.push({
+          msg: "Please select a style !",
+          type: "error",
+        });
+        return;
+      }
+      if (!fit) {
+        messages.push({
+          msg: "Please select a fit !",
+          type: "error",
+        });
+        return;
+      }
+      if (!review) {
+        messages.push({
+          msg: "Please add a review !",
+          type: "error",
+        });
+        return;
+      }
+      if (!rating) {
+        messages.push({
+          msg: "Please select a rating !",
+          type: "error",
+        });
+        return;
+      }
+
+      if (messages.length > 0) {
+        dispatch(
+          showDialog({
+            header: "Adding review error !",
+            messages,
+          })
+        );
+      } else if (images.length > 0) {
+        const imagePromises = images.map(async (img) => {
+          let blob = dataURItoBlob(img);
+          let formData = new FormData();
+
+          formData.append("path", "reviews images");
+          formData.append("file", blob);
+
+          const data = await uploadImages(formData);
+          return data;
+        });
+        uploaded_images = (await Promise.all(imagePromises)).flat(Infinity);
+      }
+      const { data } = await axios.put(`/api/product/${product._id}/reviews`, {
+        size,
+        style,
+        fit,
+        rating,
+        review,
+        images: uploaded_images,
+      });
+
+      setReviews(data.reviews);
+      setStyle("");
+      setSize("");
+      setFit("");
+      setImages([]);
+      setRating(0);
+      setReview("");
+
+      toast.success("REVIEW SUBMITTED");
+      setLoading(false);
+
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
 
   return (
     <div className={styles.reviews__add}>
-      {/* dialog modal */}
-
+      {loading && <DotLoaders loading={loading} />}
+      <DialogModal />
       <div className={styles.reviews__add_wrap}>
         <div className={styles.flex} style={{ gap: "10px" }}>
           <Select
